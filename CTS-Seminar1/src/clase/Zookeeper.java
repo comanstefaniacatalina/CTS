8581cf30e216903ee15d5e69017338e27d473c14
comanstefaniacatalina <comanstefania18@stud.ase.ro>
2021-02-27 15:09:05 +0200

    Zebra Girafa Interfata

--- CTS-Seminar1/src/clase/Zookeeper.java
+++ CTS-Seminar1/src/clase/Zookeeper.java
@@ -1,5 +1,15 @@
 package clase;
 
 public class Zookeeper {
+	private String nume;
 
+	public Zookeeper(String nume) {
+		super();
+		this.nume = nume;
+	}
+	
+	public void feed(Animal e)
+	{
+		System.out.println("Ingrijitorul "+this.nume+"hraneste animalul"+e.getNume());
+	}
 }
