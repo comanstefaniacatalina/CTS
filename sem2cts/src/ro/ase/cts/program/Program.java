1a55a81b593dd169b9fd36c4d03cdab566103f5b
comanstefaniacatalina <comanstefania18@stud.ase.ro>
2021-03-06 15:27:29 +0200

    Transformare interfata in clasa abstracta

--- sem2cts/src/ro/ase/cts/program/Program.java
+++ sem2cts/src/ro/ase/cts/program/Program.java
@@ -4,16 +4,23 @@ import java.io.FileNotFoundException;
 
 import java.util.List;
 
-import ro.ase.cts.clase.Angajat;
 import ro.ase.cts.clase.Aplicant;
 import ro.ase.cts.readers.ReaderAngajat;
+import ro.ase.cts.readers.ReaderAplicanti;
 
 public class Program {
+	
+	public static List<Aplicant> citesteAplicanti(ReaderAplicanti reader) throws FileNotFoundException
+	{
+		List<Aplicant>listaAplicanti=reader.readAplicanti();
+		return listaAplicanti;
+	}
+	
 
 	public static void main(String[] args) {
 		List<Aplicant> listaAplicanti;
 		try {
-			listaAplicanti = ReaderAngajat.readAngajati("angajati.txt");
+			listaAplicanti = citesteAplicanti(new ReaderAngajat("angajati.txt"));
 			for(Aplicant aplicant:listaAplicanti)
 				System.out.println(aplicant.toString());
 		} catch (FileNotFoundException e) {
