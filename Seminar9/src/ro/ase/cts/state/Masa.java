af9eeeb39df0437d3997dd00bb2c22ee641236df
comanstefaniacatalina <comanstefania18@stud.ase.ro>
2021-04-21 12:23:25 +0300

    State v1

---
+++ Seminar9/src/ro/ase/cts/state/Masa.java
@@ -0,0 +1,46 @@
+package ro.ase.cts.state;
+
+public class Masa {
+
+	private int cod;
+	private State stare;
+	public Masa(int cod) {
+		super();
+		this.cod = cod;
+		this.stare=new StateLibera();				
+	}
+	
+	public void rezervaMasa() {
+		if(this.stare instanceof StateLibera) {
+			System.out.println("Masa cu cod "+ cod+" e rezervare");
+			this.stare=new StateRezervata();
+			}
+			else {
+				System.out.println("Masa nu poate fi rezervata");
+			}
+		
+	}
+	
+	public void ridicaMasa() {
+		if(this.stare instanceof StateRezervata) {
+			System.out.println("Masa cu cod "+ cod+" e ocupata");
+			this.stare=new StateOcupata();}
+			else {
+				System.out.println("Masa nu poate fi ocupata");
+			}
+		
+	}
+	
+	public void elibereazaMasa() {
+		if(!(this.stare instanceof StateLibera )) {
+			System.out.println("Masa cu cod "+ cod+" este eliberata");
+			this.stare=new StateLibera();
+			}
+			else {
+				System.out.println("Masa este deja libera");
+			}
+		
+	}
+	
+	
+}
---
+++ Seminar9/src/ro/ase/cts/state/StateOcupata.java
@@ -0,0 +1,11 @@
+package ro.ase.cts.state;
+
+public class StateOcupata implements State{
+
+	@Override
+	public void descrieStare() {
+		// TODO Auto-generated method stub
+		System.out.println("e in starea ocupata");
+	}
+
+}
