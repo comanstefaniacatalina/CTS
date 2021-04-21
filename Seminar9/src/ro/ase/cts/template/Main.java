7a10e2ae050595e30cc824517ca11046461f749c
comanstefaniacatalina <comanstefania18@stud.ase.ro>
2021-04-21 13:12:29 +0300

    Template

---
+++ Seminar9/src/ro/ase/cts/template/Main.java
@@ -0,0 +1,13 @@
+package ro.ase.cts.template;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		TemplateOcupareMasa masa=new Masa();
+		
+		
+	}
+
+}
---
+++ Seminar9/src/ro/ase/cts/template/Masa.java
@@ -0,0 +1,29 @@
+package ro.ase.cts.template;
+
+public class Masa extends TemplateOcupareMasa {
+
+	@Override
+	public void curataMasa() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void asazaServetele() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void asazaTacamuri() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void invitaPersoane() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
