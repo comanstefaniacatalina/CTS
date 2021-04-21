e15466ed216ace9267f4f229694a02fbd656ebfd
comanstefaniacatalina <comanstefania18@stud.ase.ro>
2021-04-21 12:23:11 +0300

    State v1

--- Seminar9/src/ro/ase/cts/state/Main.java
+++ Seminar9/src/ro/ase/cts/state/Main.java
@@ -4,6 +4,13 @@ public class Main {
 
 	public static void main(String[] args) {
 
+		Masa masa=new Masa(1);
+		
+		masa.elibereazaMasa();
+		masa.rezervaMasa();
+		masa.rezervaMasa();
+		masa.ridicaMasa();
+		masa.elibereazaMasa();
 	}
 
 }
